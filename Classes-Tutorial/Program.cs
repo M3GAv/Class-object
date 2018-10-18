a793974a3fd19042a1bee513d96c90db8bc5903a
M3GAv <mateuszcelka@wp.pl>
2018-10-18 17:27:41 +0200

    add files

---
+++ Classes-Tutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tutorial pTutor = new Tutorial(); // <- creating a new tutorial object
+            pTutor.SetTutorial(1, ".Net"); // <- calling the set tutorial method
+
+            Console.Write(pTutor.GetTutorialID()); // <- calling the get tutorial id method
+            Console.WriteLine(pTutor.GetTutorialName()); // <- calling the get tutorial name method
+
+            Tutorial test = new Tutorial(); // <- creating a new tutorial object
+            test.SetTutorial(6, "Testing"); // <- calling the set tutorial method
+            Console.WriteLine("{0} {1}",test.GetTutorialID(),test.GetTutorialName()); // <- calling tutorial id method and tutorail name method
+        }
+    }
+}
---
+++ Classes-Tutorial/Tutorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes_Tutorial
+{
+    class Tutorial
+    {
+        int TutorialID;    // <- defining the fields of the class
+        string TutorialName; // <-    -||-
+
+        public void SetTutorial(int pID, string pName) // <- defining the set tutorial method
+        {
+            TutorialID = pID; // <- setting the fields values
+            TutorialName = pName; // <-     -||-
+        }
+
+        public String GetTutorialName() // defining the get tutorial name method
+        {
+            return TutorialName; // <- returning the TutorialName
+        }
+
+        public Int32 GetTutorialID() // <- defining the get tutorial id method
+        {
+            return TutorialID; // returning the TutorialID
+        }
+    }
+}
